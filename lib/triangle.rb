b236f666059d73dededc76a0d1c0e2c3f944f758
Jc Dior <jessicaclairdior@gmail.com>
2020-09-27 02:05:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,14 +2,14 @@ class Triangle
   attr_accessor :x, :y, :z
 
   def initializes(x, y, z)
-    @x = x 
-    @y = y 
+    @x = x
+    @y = y
     @z = z
   end
 
 def kind
   if side_1 == side_2 == side_3
-    then 
+    then
 end
 
 end
