9b33163dfa8ee1f84116876b152c82645758ed9b
Jc Dior <jessicaclairdior@gmail.com>
2020-09-27 01:59:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
-  # write code here
+  attr_accessor :kind
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+
+def kind
+end
+  
 end
